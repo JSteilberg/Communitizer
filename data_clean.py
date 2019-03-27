47bbf8c1da3ba814296d277de131f174eeb5819c
jsteilberg <steilberg.j@husky.neu.edu>
2019-03-27 13:18:56 -0400

    Made data cleaner take in a parameters file for parallelization, made data cleaner into a class

--- data_clean.py
+++ data_clean.py
@@ -24,221 +24,272 @@ import nltk
 import numpy as np
 
 from local_loader import sample_file
+from params import make_csv_dict
 
 import pdb
 
-def str_to_ngrams(string, gram_num):
-    """
-    Purpose: Returns a tuple of the n-grams for a string
-    Input: string    - String to convert into ngrams
-           gram_num  - Number of n-grams to make for the string
-    Returns: Tuple of n-grams
-    """
-    n_grams = nltk.ngrams(nltk.word_tokenize(string), gram_num)
-    return  [' '.join(n_gram) for n_gram in n_grams]
 
-def normalize(dictionary):
-    """
-    Purpose: Divides each value in a dictionary by the sum of all values
-    Input: Dictionary with number values
-    Returns: Dictionary with number values in [0..1]
-    """
-    minimum = min(dictionary.values())
-    total   = max(dictionary.values()) - minimum 
-    return {k : (v - minimum) / total \
-            for k,v in dictionary.items()}
+class DataCleaner:
 
-def data_to_grdict(data, gram_num, normalize=False):
-    """
-    Purpose: Turns a list of strings into a dictionary of n-gram occurrences
-    Input: data      - List of strings
-           gram_num  - n in n-grams (e.g. 1 => unigrams, 2 => bigrams)
-           normalize - Divide all dictionary values by the max value?
-    """
-    out_dict = dict()
+    def __init__(self, data_loc, params_loc):
+        self.vocab = []
+
+        self.params = make_csv_dict(params_loc)
+        self.data_loc = data_loc
+
+        # Put commonly used params into fields
+        self.gram_num    = self.params['gram_num']
+        self.start_word  = self.params['start_word']
+        self.stop_word   = self.params['stop_word']
+        self.sample_rate = self.params['sample_rate']
+        self.subreddit   = self.params['subreddit']
+        self.min_score   = self.params['min_score']
+
+    def load_data(self):
+        """
+        Purpose:
+        Input:
+        Output:
+        """
+        print("Loading data...")
+        self.raw_str_data = self.clean_data(
+            sample_file(self.data_loc,
+                        self.subreddit,
+                        sample_rate=self.sample_rate,
+                        flip=False,
+                        min_score=self.min_score))
+
+        print("Creating " + str(self.gram_num) + "-grams...")
+        self.ngram_dict = self.data_to_grdict(self.raw_str_data,
+                                              self.gram_num,
+                                              normalize=False)
+
+        print("Creating vocabulary vector")
+        self.vocabulary = self.make_voc_vec(self.ngram_dict)
+        
+    def str_to_ngrams(self, string, gram_num):
+        """
+        Purpose: Returns a tuple of the n-grams for a string
+        Input: string    - String to convert into ngrams
+               gram_num  - Number of n-grams to make for the string
+        Returns: Tuple of n-grams
+        """
+        n_grams = nltk.ngrams(nltk.word_tokenize(string), gram_num)
+        return [' '.join(n_gram) for n_gram in n_grams]
+
+    def normalize(self, dictionary):
+        """
+        Purpose: Divides each value in a dictionary by the sum of all values
+        Input: Dictionary with number values
+        Returns: Dictionary with number values in [0..1]
+        """
+        minimum = min(dictionary.values())
+        total   = max(dictionary.values()) - minimum 
+        return {k : (v - minimum) / total \
+                for k,v in dictionary.items()}
+
+    def data_to_grdict(self, data, gram_num, normalize=False):
+        """
+        Purpose: Turns a list of strings into a dictionary of n-gram occurrences
+        Input: data      - List of strings
+               gram_num  - 'n' in n-gram
+               normalize - Divide all dictionary values by the max value?
+        
+        """
+        out_dict = dict()
+        
+        for comment in data:
+            for n_gram in self.str_to_ngrams(comment, gram_num):
+                if n_gram in out_dict:
+                    out_dict[n_gram] += 1
+                else:
+                    out_dict[n_gram] = 1
+
+        if normalize:
+            out_dict = normalize(out_dict)
+
+        return out_dict
+
+    def data_to_grvec(self, data, vocabulary, gram_num, normalize):
+        """
+        Purpose: Turns a list of strings into a list of n-gram vectors
+        Input: data       - List of strings (bodies of comments)
+               vocabulary - List of strings constituting words in the vocabulary
+               gram_num   - n in n-grams
+               normalize  - Normalize each vector to be a unit vector?
+        Output: 
+        """
+        outvec = np.zeros((len(data), len(vocabulary)))
+
+        for i in range(len(data)):
+            outvec[i] = comm_to_grvec(data[i], vocabulary, gram_num, normalize)
+
+        return outvec
+
+    def comm_to_grvec(self, comment, vocabulary, gram_num, normalize):
+        """
+        Purpose: Takes a single cleaned comment and converts it into a vector of size
+                  len(vocabulary).
+        Input: comment    - Cleaned comment string
+               vocabulary - List of strings constituting words in the vocabulary
+               gram_num   - N in n-grams for the vocabulary
+               normalize  - If False, returns vector where 1 in position i indicates 
+                            presence of a n-gram in position i in the vocabulary
+                            If True, normalizes the vector so that it is a unit vector 
+        Returns: Vector of size len(vocabulary)
+        """
+        # Allocate the output vector
+        gram_vec = np.zeros(len(vocabulary))
     
-    for comment in data:
-        for n_gram in str_to_ngrams(comment, gram_num):
-            if n_gram not in out_dict:
-                out_dict[n_gram] = 1
+        n_grams = [gr for gr in self.str_to_ngrams(comment, gram_num)]
+    
+        # Loop through all the n-grams
+        for gram in n_grams:
+            if gram in vocabulary:
+                gram_vec[vocabulary.index(gram)] = 1
+                # Presence vs not presence probably less noisy than total #
+                # gram_vec[vocabulary.index(asonestr)] += 1
             else:
-                out_dict[n_gram] += 1
-
-    if normalize:
-        out_dict = normalize(out_dict)
-
-    return out_dict
-
-def data_to_grvec(data, gram_num, vocabulary, normalize):
-    """
-    Purpose: Turns a list of strings into a list of n-gram vectors
-    Input: data      - List of strings (bodies of comments)
-           gram_num  - n in n-grams
-           normalize - Normalize each vector to be a unit vector?
-    Output: 
-    """
-    outvec = np.zeros((len(data), len(vocabulary)))
-
-    for i in range(len(data)):
-        outvec[i] = comm_to_grvec(data[i], gram_num, vocabulary, normalize)
-
-    return outvec
-
-def make_vocab(subreddit_dict, overall_dict):
-    pass
-
-def comm_to_grvec(comment, gram_num, vocabulary, normalize):
-    """
-    Purpose: Takes a single cleaned comment and converts it into a vector of size
-             len(vocabulary).
-    Input: comment    - Cleaned comment string
-           gram_num   - Number of ngrams to use when generating vector
-           vocabulary - Vocabulary of n-grams 
-           normalize  - If False, returns vector where 1 in position i indicates 
-                        presence of a n-gram in position i in the vocabulary
-                        If True, normalizes the vector so that it is a unit vector 
-    Returns: Vector of size len(vocabulary)
-    """
-    # Allocate the output vector
-    gram_vec = np.zeros(len(vocabulary))
-
-    n_grams = [gr for gr in str_to_ngrams(comment, gram_num)]
-
-    # Loop through all the n-grams
-    for gram in n_grams:
-        if gram in vocabulary:
-            gram_vec[vocabulary.index(gram)] = 1
-            # Presence vs not presence probably more important than total #
-            # gram_vec[vocabulary.index(asonestr)] += 1
+                #print(asonestr)
+                pass
+    
+        return gram_vec
+    
+    def clean_data(self, data):
+        """
+        Purpose: Given a list of comments, cleans each one
+        Input: List of dictionaries corresponding to comments
+        Output: List of strings len of which is <= to the len of the input
+        """
+        out_data = list()
+        for comment in data:
+            comment = self.clean_comment(comment)
+            if comment != '':
+                out_data.append(comment)
+    
+        return out_data
+    
+    def clean_comment(self, comment):
+        """
+        Purpose: Cleans a given comment, adding stop and start and stop symbols. 
+        Input: Comment to be cleaned, as a dictionary
+        Returns: Clean comment as a string
+                 Returns an empty string if the entire comment was trash
+        """
+        comment = self.start_word + " " \
+            + comment['body'].lower() + " " \
+            + self.stop_word
+        comment = comment.replace("deleted", " ")
+        comment = comment.replace("removed", " ")
+    
+        comment = re.sub('[^0-9a-zA-Z/\:\'\.\+\- ]+', ' ', comment)
+    
+        #comment = comment.replace("gt", " ")
+        #comment = comment.replace("amp", " ")
+        
+        comment = ' '.join(comment.split())
+        if len(comment) < 8:
+            return ''
         else:
-            #print(asonestr)
-            pass
+            return comment
 
-    return gram_vec
-
-def clean_data(data):
-    """
-    Purpose: Given a list of comments, cleans each one
-    Input: List of dictionaries corresponding to comments
-    Output: List of strings len of which is <= to the len of the input
-    """
-    out_data = list()
-    for comment in data:
-        comment = clean_comment(comment)
-        if comment != '':
-            out_data.append(comment)
-
-    return out_data
-
-def clean_comment(comment):
-    """
-    Purpose: Cleans a given comment, adding stop and start and stop symbols. 
-    Input: Comment to be cleaned, as a dictionary
-    Returns: Clean comment as a string
-             Returns an empty string if the entire comment was trash
-    """
-    comment = "|st1 " + comment['body'].lower() + " sp1#"
-    comment = comment.replace("deleted", " ")
-    comment = comment.replace("removed", " ")
-
-    comment = re.sub('[^0-9a-zA-Z/\:\'\.\+\- ]+', ' ', comment)
-
-    #comment = comment.replace("gt", " ")
-    #comment = comment.replace("amp", " ")
+    # @@@@@@@@@@ DEPRECATED @@@@@@@@@@
+    def filter_comments(self, data, filt_field, value):
+        """
+        Purpose: Filters comments, returning those who match the input requirements.
+        Input: data       - List of comments (dictionaries)
+               filt_field - Field in the comment dictionary to filter on 
+               value      - Value for the field
+        Returns: List of comments that meet given criteria
+        """
+        if type(data) == str:
+            comms = [k for k in data if k[filt_field].lower() == value]
+        else:
+            comms = [k for k in data if k[filt_field] == value]
     
-    comment = ' '.join(comment.split())
-    if len(comment) < 8:
-        return ''
-    else:
-        return comment
-
-def filter_comments(data, filt_field, value):
-    """
-    Purpose: Filters comments, returning those who match the input requirements.
-    Input: data       - List of comments (dictionaries)
-           filt_field - Field in the comment dictionary to filter on 
-           value      - Value for the field
-    Returns: List of comments that meet given criteria
-    """
-    if type(data) == str:
-        comms = [k for k in data if k[filt_field].lower() == value]
-    else:
-        comms = [k for k in data if k[filt_field] == value]
-
-    newcoms = list()
-    for comm in comms:
-        if '[deleted]' in comm['body'] \
-           or '[removed]' in comm['body'] \
-           or 'st1 spl' in comm['body']:
-            continue
-
-        comm['body'] = clean_comment(comm)
-        newcoms.append(comm)
-
-    return newcoms
-
-def print_top(dictionary, num):
-    """
-    Purpose: Prints the top num elements of a dictionary, sorted by descending value
-    Input: dictionary - Dictionary to print
-           num        - Number of top values to print
-    """
-    astoops = [(k, dictionary[k]) for k in dictionary]
-    astoops = sorted(astoops, key=lambda x: x[1], reverse=True)
-
-    for i in range(min(num, len(dictionary))):
-        print("{0:0.5f}".format(astoops[i][1]) \
-              + "\t" + astoops[i][0])
-
-def main():
-    data_file = "./test.dat"
-    subreddit = "programming"
-    gram_num = 1
+        newcoms = list()
+        for comm in comms:
+            if '[deleted]' in comm['body'] \
+               or '[removed]' in comm['body'] \
+               or (self.start_word + ' ' + self.stop_word) in comm['body']:
+                continue
     
-    print("Loading data...")
-    data = sample_file(data_file, subreddit, sample_rate=1, flip=False, min_score=0)
-    data = clean_data(data)
-    gr_dict = data_to_grdict(data, 1, normalize=False)
-    gr_vec = data_to_grvec(data, 1, gr_dict.keys(), False)
+            comm['body'] = clean_comment(comm)
+            newcoms.append(comm)
 
-    print("Creating ngram dictionaries")
-    #gen_dict = 
+        return newcoms
+
+    def print_top(self, num):
+        """
+        Purpose: Prints the top num elements of a dictionary, sorted by descending value
+        Input: Number of top values to print
+        """
+        astoops = [(k, dictionary[k]) for k in dictionary]
+        astoops = sorted(astoops, key=lambda x: x[1], reverse=True)
     
-    pdb.set_trace()
-    gen_norm = normalize(general_dict)
-    general_dict.clear()
-    print("Size of general dictionary: " + str(len(gen_norm)))
+        for i in range(min(num, len(dictionary))):
+            print("{0:0.5f}".format(astoops[i][1]) \
+                  + "\t" + astoops[i][0])
     
-    print("General reddit: ")
-    print_top(gen_norm, 35)
+    def make_voc_vec(self, dictionary):
+        """
+        Purpose: Creates a vocabulary vector from a given dictionary
+        Input: Dictionary (Usually of unigram frequencies, NOT normalized)
+        Output: List of words, sorted
+        """
+    
+        min_freq   = self.params['min_word_count']
+        #remove_avg = clean_params['mean_subtract_grams']
+    
+        voc_vec = list()
+        for elem in dictionary:
+            if dictionary[elem] > min_freq:
+                voc_vec.append(elem)
+    
+        return sorted(voc_vec)
+    
+    
+    def file_to_grams(data_file_name, params):
+        """
+    
+    
+        """
+        pass
+    
+    
+    def main():
+        
+        #print("\n/r/" + subreddit + ": ")
+        #print_top(common, 35)
 
-    #print("\n/r/" + subreddit + ": ")
-    #print_top(common, 35)
+        #prog_comms = filter_comments(data, 'subreddit', 'politics')
+        #funn_comms = filter_comments(data, 'subreddit', 'programming')
 
-    #prog_comms = filter_comments(data, 'subreddit', 'politics')
-    #funn_comms = filter_comments(data, 'subreddit', 'programming')
+        #vocab = sorted([k for k in gen_norm.keys()])
+        
+        #print("Making comment vectors")
+        #prog_vecs = [make_single_comm_grvec(comm, 1, vocab) for comm in prog_comms[:50]]
+        #funn_vecs = [make_single_comm_grvec(comm, 1, vocab) for comm in funn_comms[:50]]
 
-    #vocab = sorted([k for k in gen_norm.keys()])
+        prog_vecs =([vec / sum(vec) for vec in prog_vecs])
+        funn_vecs =([vec / sum(vec) for vec in funn_vecs])
+        
+        tot = prog_vecs + funn_vecs
+        
+        print("Fitting spherical kmeans")
+        skm = SphericalKMeans(n_clusters=2)
+        skm.fit(tot)
 
-    #print("Making comment vectors")
-    #prog_vecs = [make_single_comm_grvec(comm, 1, vocab) for comm in prog_comms[:50]]
-    #funn_vecs = [make_single_comm_grvec(comm, 1, vocab) for comm in funn_comms[:50]]
-
-    prog_vecs =([vec / sum(vec) for vec in prog_vecs])
-    funn_vecs =([vec / sum(vec) for vec in funn_vecs])
-
-    tot = prog_vecs + funn_vecs
-
-    print("Fitting spherical kmeans")
-    skm = SphericalKMeans(n_clusters=2)
-    skm.fit(tot)
-
-    for idx,comm in enumerate(prog_comms[:50] + funn_comms[:50]):
-        print(skm.labels_[idx], ":",comm['body'][:100])
+        for idx,comm in enumerate(prog_comms[:50] + funn_comms[:50]):
+            print(skm.labels_[idx], ":",comm['body'][:100])
 
         
 
-    pdb.set_trace()
-        
-main()
+        pdb.set_trace()
+    
+fleeb = DataCleaner('./data/RC_2007-02', './cfg/clean_params/clean_params.csv')
+
+fleeb.load_data()
+
+pdb.set_trace()
 
---
+++ params.py
@@ -0,0 +1,49 @@
+################################################################################
+# Purpose: Turns a csv file into a dictionary
+# Authors: Jack Steilberg <jsteilberg@ccis.neu.edu>,
+#          Sajid Raihan   <raihan.s@husky.neu.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+################################################################################
+
+
+import csv
+from pydoc import locate
+
+def make_csv_dict(file_name):
+    """
+    Purpose: Converts file specified by file_name into a dictionary
+    Input: String containing path to a csv file
+    Output: Dictionary
+    """
+    out_dict = dict()
+
+    with open(file_name) as csv_file:
+        reader = csv.reader(csv_file)
+
+        labels = [col.lower() for col in reader.__next__()]
+        tipe_idx = labels.index('datatype')
+        name_idx = labels.index('name')
+        val_idx = labels.index('value')
+
+        for row in reader:
+            name = row[name_idx]
+
+            # Get the type for the value and cast
+            tipe = locate(row[tipe_idx])
+            value = tipe(row[val_idx])
+
+            out_dict[name] = value
+
+    return out_dict
